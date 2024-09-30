4cc5dde09b2514eb0fbfaad462033761ce994568
royaloakks <adamroyal@gmail.com>
2024-09-30 21:27:13 +0100

    Add Pixeloid Sans font to landing page

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ document.addEventListener('DOMContentLoaded', () => {
     const overlay = document.querySelector('.overlay');
     const duration = 60 * 1000; // 1 minute in milliseconds
 
-    // Set a timeout to gradually reduce the overlay opacity
+    // Set a timeout to gradually reduce the overlay blur
     setTimeout(() => {
-        overlay.style.opacity = 0; // Reveal the background by reducing overlay opacity
+        overlay.style.filter = 'blur(0px)'; // Remove the blur effect
     }, 1000); // Start after 1 second
 
     // Optional: If you want to ensure the transition takes 1 minute
     setTimeout(() => {
-        overlay.style.transition = `opacity ${duration}ms ease`; // Set transition duration
+        overlay.style.transition = `filter ${duration}ms ease`; // Set transition duration
     }, 1000); // Start transition after 1 second
 });
